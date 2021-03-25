17e018cb50d7fb433aa39bcfe2c8b768470a7725
朱宝华 <897332413@qq.com>
2021-03-25 21:00:56 +0800

    zookeeper代替Eureka实现服务注册与发现（@EnableDiscoveryClient）

--- cloud2020/cloud-consumerzk-order80/src/main/java/com/atguigu/springcloud/OrderZK80.java
+++ cloud2020/cloud-consumerzk-order80/src/main/java/com/atguigu/springcloud/OrderZK80.java
@@ -3,18 +3,16 @@ package com.atguigu.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Auther: garrick
- * @Date: 2021/3/24 15:47
+ * @Date: 2021/3/25 20:45
  * @Description:
  */
 @SpringBootApplication
-@EnableEurekaClient
 @EnableDiscoveryClient
-public class PaymentMain8001 {
+public class OrderZK80 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class,args);
+        SpringApplication.run(OrderZK80.class,args);
     }
 }
--- cloud2020/cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/PaymentMain8004.java
+++ cloud2020/cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/PaymentMain8004.java
@@ -3,18 +3,16 @@ package com.atguigu.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Auther: garrick
- * @Date: 2021/3/24 15:47
+ * @Date: 2021/3/25 20:04
  * @Description:
  */
 @SpringBootApplication
-@EnableEurekaClient
-@EnableDiscoveryClient
-public class PaymentMain8001 {
+@EnableDiscoveryClient  //该注解用于向使用consul或者zookeeper作为注册中心时注册服务
+public class PaymentMain8004 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class,args);
+        SpringApplication.run(PaymentMain8004.class,args);
     }
 }
