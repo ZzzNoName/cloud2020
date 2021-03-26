507db750cf52adda5d0318f980e5d2ddd53ad914
朱宝华 <897332413@qq.com>
2021-03-26 09:57:41 +0800

    restTemplate中getForObject，ResponseEntity比较，修改7001Eureka为单体，去除集群，方便测试

--- cloud2020/cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud2020/cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,9 +29,19 @@ public class OrderController {
     public CommonResult<Payment> create(Payment payment) {
         return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
     }
-
+    //restTemplate中getForObject，返回对象为响应体中数据转换为对象，基本上可以理解为json
     @GetMapping("/consumer/payment/get/{id}")
-    public CommonResult<Payment> create(@PathVariable Long id) {
+    public CommonResult<Payment> getPayment(@PathVariable Long id) {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+    //返回对象为ResponseEntity对象，包含了响应中的一些重要信息，如响应头，状态码，响应体等
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable Long id){
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if(entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        }else{
+            return new CommonResult<>(444,"操作失败");
+        }
     }
 }
