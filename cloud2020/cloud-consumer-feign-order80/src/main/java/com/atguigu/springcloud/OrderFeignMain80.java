8c72972551c7714360c42989c1e4759ac0b3635b
朱宝华 <897332413@qq.com>
2021-03-26 10:41:03 +0800

    OpenFeign使用步骤  -- 面向接口 @EnableFeignClients @FeignClient(value = "CLOUD-PAYMENT-SERVICE")

---
+++ cloud2020/cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderFeignMain80.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @Auther: garrick
+ * @Date: 2021/3/26 10:29
+ * @Description:
+ * Feigin默认自带负载均衡功能
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class OrderFeignMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFeignMain80.class,args);
+    }
+}
