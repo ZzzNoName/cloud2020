1fae90bea96b05b90ef60e1738aba1259122b421
朱宝华 <897332413@qq.com>
2021-03-31 17:05:23 +0800

    @DefaultProperties(defaultFallback = "payment_Global_FallbackMethod") 提取公共的服务降级措施，减少代码膨胀

--- cloud2020/cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud2020/cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -1,6 +1,8 @@
 package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @Slf4j
+@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod")
 public class PaymentController {
     @Autowired
     private PaymentService paymentService;
@@ -30,7 +33,7 @@ public class PaymentController {
         log.info("****result: "+result);
         return result;
     }
-
+    @HystrixCommand //加了@DefaultProperties属性注解，并且没有写具体方法名字，就用统一全局的
     @GetMapping("/payment/hystrix/timeout/{id}")
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id) throws InterruptedException
     {
@@ -39,4 +42,8 @@ public class PaymentController {
         return result;
     }
 
+    public String payment_Global_FallbackMethod()
+    {
+        return "Global异常处理信息，请稍后再试，/(ㄒoㄒ)/~~";
+    }
 }
