0ab46b5b3cb462a5db95b53c3c0d30e505e5fb4a
朱宝华 <897332413@qq.com>
2021-04-09 13:43:54 +0800

    Predicate&Filter练习 + 自定义过滤器

--- cloud2020/cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/GateWayMain9527.java
+++ cloud2020/cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/GateWayMain9527.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import java.time.ZonedDateTime;
+
 /**
  * @Auther: garrick
  * @Date: 2021/4/9 12:58
@@ -15,5 +17,9 @@ public class GateWayMain9527 {
     public static void main(String[] args)
     {
         SpringApplication.run(GateWayMain9527.class,args);
+//        2021-04-09T13:25:23.633+08:00[Asia/Shanghai]
+//        ZonedDateTime zbj = ZonedDateTime.now(); // 默认时区
+//        System.out.println(zbj);
     }
+
 }
