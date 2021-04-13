6ff03f295018a17e82d5d353868edce1da77a72f
朱宝华 <897332413@qq.com>
2021-04-13 15:23:28 +0800

    消息驱动之生消费者

---
+++ cloud2020/cloud-stream-rabbitmq-consumer8803/src/main/java/com/atguigu/springcloud/StreamMQMain8803.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: garrick
+ * @Date: 2021/4/13 15:20
+ * @Description:
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(StreamMQMain8803.class,args);
+    }
+}
