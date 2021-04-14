f880a92847fd0117088b2735c7fe8a27c4d1f0fe
朱宝华 <897332413@qq.com>
2021-04-14 15:35:54 +0800

    Nacos作为配置中心 基础配置

---
+++ cloud2020/cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Auther: garrick
+ * @Date: 2021/4/14 15:27
+ * @Description:
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
