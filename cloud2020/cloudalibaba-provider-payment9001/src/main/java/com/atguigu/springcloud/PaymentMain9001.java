688df3f4f2cd9bd6c2001cf2b25229d3bc43a000
朱宝华 <897332413@qq.com>
2021-04-14 15:06:30 +0800

    Nacos作为服务的提供者

---
+++ cloud2020/cloudalibaba-provider-payment9001/src/main/java/com/atguigu/springcloud/PaymentMain9001.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Auther: garrick
+ * @Date: 2021/4/14 15:01
+ * @Description:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+}
