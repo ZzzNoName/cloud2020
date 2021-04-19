0784de3320e66d18b3ff66768fb93dc93583070d
朱宝华 <897332413@qq.com>
2021-04-19 22:02:17 +0800

    按资源名称限流-后续处理

--- cloud2020/cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
+++ cloud2020/cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
@@ -67,6 +67,7 @@ public class FlowLimitController {
     public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
                              @RequestParam(value = "p2",required = false) String p2){
         log.info(p1+"==="+p2);
+//        int age = 10/0;
         return "-----testHotKey";
     }
     public String dealHandler_testHotKey(String p1,String p2,BlockException e){
